bf532a11dd3de03085d9909d0cfd52fee28d8c36
Pedro Abraão <pedro.abraao@mybp.com.br>
2022-03-30 22:08:39 -0300

    [MYBP-26]

--- resources/js/g/admissao/processo/app.js
+++ resources/js/g/admissao/processo/app.js
@@ -22,7 +22,7 @@ const app = new Vue({
     data: {
         tituloJanela: "Admissão",
         preload: false,
-        editando: false,
+        editando: true,
         apagado: false,
         cadastrado: false,
         cadastrando: false,
@@ -875,7 +875,7 @@ const app = new Vue({
         },
         carregou(dados) {
             this.lista = dados.itens;
-            this.editando = dados.admissao_processo_dados_editar;
+            // this.editando = true;
             this.selecionaTudo = this.tudoMarcado;
             this.controle.carregando = false;
         },
