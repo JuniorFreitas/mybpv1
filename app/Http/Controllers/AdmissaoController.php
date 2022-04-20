c28a865daea5c7dd136b391cb6ac63ea037a9fac
Júnior Freitas <josedejesusjunior@gmail.com>
2022-04-20 18:47:50 -0300

    Exportacao

--- app/Http/Controllers/AdmissaoController.php
+++ app/Http/Controllers/AdmissaoController.php
@@ -126,7 +126,6 @@ class AdmissaoController extends Controller
 
         $tipo_admissao = [
             'TEMPORARIO',
-            'INTERMITENTE',
             'DETERMINADO'
         ];
 
@@ -449,10 +448,13 @@ class AdmissaoController extends Controller
         } catch (\Exception $e) {
             DB::rollback();
             $msg = "error ADMISSAO AVULSA STORE: {$e->getFile()} , {$e->getMessage()} , {$e->getCode()}, {$e->getLine()} | Usuario: " . User::find(auth()->id())->nome;
-            \Log::debug($dados);
             \Log::debug($msg);
-            return response()->json(['msg' => $msg], 400);
-//            return response()->json(['msg' => 'Houve um erro por favor tente novamente!'], 400);
+            \Log::debug($e->getTraceAsString());
+            \Log::info("-------DADOS-------");
+            Sistema::telegram(print_r($dados, true));
+            \Log::info("-------FIM DE DADOS-------");
+            return response()->json(['msg' => 'Houve um erro por favor tente novamente, Caso persista entre em contato com o suporte!'], 400);
+
         }
 
     }
@@ -921,10 +923,9 @@ class AdmissaoController extends Controller
                 \Log::debug($msg);
                 \Log::debug($e->getTraceAsString());
                 \Log::info("-------DADOS-------");
-                \Log::alert($dados);
+                Sistema::telegram(print_r($dados, true));
                 \Log::info("-------FIM DE DADOS-------");
-                return response()->json(['msg' => $msg], 400);
-//                return response()->json(['msg' => 'Houve um erro por favor tente novamente!'], 400);
+                return response()->json(['msg' => 'Houve um erro por favor tente novamente, Caso persista entre em contato com o suporte!'], 400);
             }
         }
     }
