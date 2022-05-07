d8bc8ac43224acf78623fd78a63e120f5deeaada
Júnior Freitas <josedejesusjunior@gmail.com>
2022-05-07 11:27:58 -0300

    novos ajustes faltando treinamento e portaria

--- resources/js/components/planejamento/mobilizacao/index.vue
+++ resources/js/components/planejamento/mobilizacao/index.vue
@@ -22,17 +22,101 @@
         <div class="row" v-if="!preload && showRelatorio">
             <div class="col-12">
                 <div class="text-center">
-                    <h5>Projeto: {{ dados.projeto.nome }}</h5>
-                    <p>{{ dados.projeto.preenchidas }} preenchida(s) de {{ dados.projeto.qnt_total }} vaga(s)</p>
+                    <h5 class="text-uppercase">Projeto: {{ dados.projeto.nome }}</h5>
+                    <pre>{{ dados.projeto.preenchidas }} preenchida(s) de {{ dados.projeto.qnt_total }} vaga(s)</pre>
                 </div>
             </div>
 
-            <div class="col-12">
-                <div class="table-responsive">
+            <div class="col-12 p-3" v-for="(item, index) in dados.vagas_projeto">
+                <div class="text-center">
+                    <h5>{{ item.vaga_aberta.titulo }}<br>
+                        <pre>({{ item.vaga_aberta.vaga.nome }})</pre>
+                    </h5>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-6">
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Total: <strong>
+                                {{ item.qnt_total }}
+                            </strong></li>
+                            <li class="list-group-item">Preenchidas: <strong>
+                                {{ item.qnt_preenchida }}
+                            </strong></li>
+                            <li class="list-group-item">Em Processo de Seleção: <strong>
+                                {{ item.em_processo_selecao }}
+                            </strong></li>
+                            <li class="list-group-item">Treinamento Fase 1: <strong>
+                                Treinamento Fase 1
+                            </strong></li>
+                            <li class="list-group-item">Pendente Treinamento: <strong>
+                                {{ item.status_pendente_treinamento }}
+                            </strong></li>
+                            <li class="list-group-item">Exames: <strong>
+                                {{ item.status_encaminhado_exame }}
+                            </strong></li>
+                            <li class="list-group-item">Portaria: <strong>
+                                Portaria
+                            </strong></li>
+                            <li class="list-group-item">Admitidos: <strong>
+                                {{ item.status_admitido }}
+                            </strong></li>
+                            <li class="list-group-item">Ag. Qualificação: <strong>
+                                {{ item.status_aguardando_qualificacao }}
+                            </strong></li>
+                            <li class="list-group-item">ASO no Ambulatório: <strong>
+                                {{ item.status_aso_no_ambulatorio }}
+                            </strong></li>
+                            <li class="list-group-item">Cancelado: <strong>
+                                {{ item.status_cancelado }}
+                            </strong></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-6">
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Desistências: <strong>
+                                {{ item.status_desistencia }}
+                            </strong></li>
+                            <li class="list-group-item">Encaminhado para Exames: <strong>
+                                {{ item.status_encaminhado_exame }}
+                            </strong></li>
+                            <li class="list-group-item">Pendente ASO: <strong>
+                                {{ item.status_pendente_aso }}
+                            </strong></li>
+                            <li class="list-group-item">Pendente Documentos: <strong>
+                                {{ item.status_pendente_documento }}
+                            </strong></li>
+                            <li class="list-group-item">Pronto para admissão: <strong>
+                                {{ item.status_pronto_para_admissao }}
+                            </strong></li>
+                            <li class="list-group-item">Admitido: <strong>
+                                {{ item.status_admitido }}
+                            </strong></li>
+                            <li class="list-group-item">StandBy: <strong>
+                                {{ item.status_standby }}
+                            </strong></li>
+                            <li class="list-group-item">Admissão tipo determinada: <strong>
+                                {{ item.tipo_admissao_determinado }}
+                            </strong></li>
+                            <li class="list-group-item">Admissão tipo fixo: <strong>
+                                {{ item.tipo_admissao_fixo }}
+                            </strong></li>
+                            <li class="list-group-item">Admissão tipo intermitente: <strong>
+                                {{ item.tipo_admissao_intermitente }}
+                            </strong></li>
+                            <li class="list-group-item">Admissão Temporária: <strong>
+                                {{ item.tipo_admissao_temporario }}
+                            </strong></li>
+                        </ul>
+                    </div>
+                </div>
+
+
+                <div v-if="false" class="table-responsive p-2">
                     <table class="table table-striped table-bordered table-hover">
                         <thead>
-                        <tr>
-                            <th class="text-center">Vaga/Cargo</th>
+                        <tr class="table-secondary">
+                            <!--                            <th class="text-center">Vaga/Cargo</th>-->
                             <th class="text-center">Total</th>
                             <th class="text-center">Preenchida</th>
                             <th class="text-center">Em Processo de Seleção</th>
@@ -58,10 +142,10 @@
                         </tr>
                         </thead>
                         <tbody>
-                        <tr v-for="(item, index) in dados.vagas_projeto">
-                            <td class="text-center">{{ item.vaga_aberta.titulo }}<br>
-                                ({{ item.vaga_aberta.vaga.nome }})<br>
-                            </td>
+                        <tr class="table-light">
+                            <!--                            <td class="text-center">{{ item.vaga_aberta.titulo }}<br>-->
+                            <!--                                ({{ item.vaga_aberta.vaga.nome }})<br>-->
+                            <!--                            </td>-->
                             <td class="text-center">{{ item.qnt_total }}</td>
                             <td class="text-center">{{ item.qnt_preenchida }}</td>
                             <td class="text-center">{{ item.em_processo_selecao }}</td>
@@ -91,24 +175,38 @@
             </div>
 
             <div class="col-12">
-                <h5>Resumo de mobilização geral:</h5>
-                <div class="alert alert-info p-2 resumo">
-                    <p>Total de curriculos cadastrados: <strong>{{ dados.total_geral_curriculos }}</strong></p>
-                    <p>Total de mobilizados: <strong>{{ dados.total_geral_curriculos_feedbacks }}</strong></p>
-                    <p>Total de curriculos selecionados: <strong>{{ dados.total_geral_curriculos_selecionados
-                        }}</strong></p>
-                    <p>Total de curriculos selecionados: <strong>{{ dados.total_geral_curriculos_selecionados
-                        }}</strong></p>
-                    <p>Total de curriculos standby: <strong>{{ dados.total_geral_curriculos_standby }}</strong></p>
-                    <p>Total de curriculos em Parecer RH: <strong>{{ dados.total_em_parecer_rh }}</strong></p>
-                    <p>Total de curriculos em Parecer Rota - Transporte: <strong>{{ dados.total_em_parecer_rota
-                        }}</strong></p>
-                    <p>Total de curriculos em Parecer Entrevista Técnica: <strong>{{ dados.total_em_parecer_tecnica
-                        }}</strong></p>
-                    <p>Total de curriculos em Parecer Teste Prático: <strong>{{ dados.total_em_parecer_teste }}</strong>
-                    </p>
-                    <p>Total de curriculos em Resultado Integrado: <strong>{{ dados.total_em_resultado_integrado
-                        }}</strong></p>
+                <div class="row">
+                    <div class="col-md-6">
+                        <h5 class="mb-2">Resumo geral mobilização:</h5>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Total de currículos cadastrados:
+                                <strong>{{ dados.total_geral_curriculos }}</strong></li>
+                            <li class="list-group-item">Total de mobilizados:
+                                <strong>{{ dados.total_geral_curriculos_feedbacks }}</strong></li>
+                            <li class="list-group-item">Total de currículos selecionados:
+                                <strong>{{ dados.total_geral_curriculos_selecionados
+                                    }}</strong></li>
+                            <li class="list-group-item">Total de currículos selecionados:
+                                <strong>{{ dados.total_geral_curriculos_selecionados
+                                    }}</strong></li>
+                            <li class="list-group-item">Total de currículos standby:
+                                <strong>{{ dados.total_geral_curriculos_standby }}</strong></li>
+                            <li class="list-group-item">Total de currículos em Parecer RH:
+                                <strong>{{ dados.total_em_parecer_rh }}</strong></li>
+                            <li class="list-group-item">Total de currículos em Parecer Rota - Transporte:
+                                <strong>{{ dados.total_em_parecer_rota
+                                    }}</strong></li>
+                            <li class="list-group-item">Total de currículos em Parecer Entrevista Técnica:
+                                <strong>{{ dados.total_em_parecer_tecnica
+                                    }}</strong></li>
+                            <li class="list-group-item">Total de currículos em Parecer Teste Prático:
+                                <strong>{{ dados.total_em_parecer_teste }}</strong>
+                            </li>
+                            <li class="list-group-item">Total de currículos em Resultado Integrado:
+                                <strong>{{ dados.total_em_resultado_integrado
+                                    }}</strong></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
@@ -172,7 +270,7 @@ export default {
 </script>
 
 <style scoped>
-.table thead th {
+.table thead th, .table th, .table td {
     vertical-align: middle;
 }
 
