7930e33922985a4d167a3ace6aca1d9eeb2adec5
Júnior Freitas <josedejesusjunior@gmail.com>
2022-06-10 04:09:36 -0300

    correcao de ASOS e Rotina

--- app/Jobs/Admissao/Processo/VencimentoAsoJob.php
+++ app/Jobs/Admissao/Processo/VencimentoAsoJob.php
@@ -49,83 +49,68 @@ class VencimentoAsoJob implements ShouldQueue
     public function handle()
     {
         try {
-            $listaDeEmprasaID = Sistema::listaEmpresas();
+            $ClientesConfigs = ClienteConfig::get();
 
-            foreach ($listaDeEmprasaID as $empresa_id) {
+            foreach ($ClientesConfigs as $cliente_config) {
+                $periodo_vencimento = ClienteConfig::LISTA_VENCIMENTOS[$cliente_config->vencimento_aso];
+                $empresa_id = $cliente_config->cliente_id;
 
-                $cliente = ClienteConfig::whereClienteId($empresa_id)->first();
-                $periodo_vencimento = ClienteConfig::LISTA_VENCIMENTOS[$cliente->vencimento_aso];
+                $periodo_vencimento = preg_replace("/[^0-9]/", "", $periodo_vencimento);
+                $data = new DataHora();
+                $data->addDia($periodo_vencimento);
 
-                if (!empty($cliente)) {
-                    $periodo_vencimento = 365 - preg_replace("/[^0-9]/", "", $periodo_vencimento);
-                    $data = new DataHora();
-                    $data->addDia($periodo_vencimento);
+                $usuarios = User::whereEmpresaId($empresa_id)
+                    ->select(['id', 'nome', 'login'])
+                    ->whereAtivo(true)
+                    ->whereHas('UserRecebeEmail', function ($q) {
+                        $q->where('nome', TipoRecebeEmail::VENCIMENTO_ASO)->where('ativo', true);
+                    })
+                    ->with(['UserRecebeEmail' => function ($q) {
+                        $q->where('nome', TipoRecebeEmail::VENCIMENTO_ASO)->where('ativo', true);
+                    }])->get();
 
-                    $usuarios = User::whereEmpresaId($empresa_id)
-                        ->select(['id', 'nome', 'login'])
-                        ->whereAtivo(true)
-                        ->whereHas('UserRecebeEmail', function ($q) {
-                            $q->where('nome', TipoRecebeEmail::VENCIMENTO_ASO);
-                            $q->where('ativo', true);
-                        })
-                        ->with(['UserRecebeEmail' => function ($q) {
-                            $q->where('nome', TipoRecebeEmail::VENCIMENTO_ASO);
-                            $q->where('ativo', true);
-                        }]);
-
-                    $data_ano = new DataHora();
-                    $data_ano->addDia(365);
-
+                foreach ($usuarios as $usuario) {
                     $AdmissoesAso = AdmissaoAso::whereAtivo(true)->whereEmpresaId($empresa_id)
-                        ->whereBetween('data_vencimento', [$data->dataInsert(), $data_ano->dataInsert()])
-                        ->where('data_vencimento', '>=', $data->dataInsert())
-                        ->where('data_vencimento', '<=', $data_ano->dataInsert())
-                        ->with('Admissao:id,feedback_id')
-                        ->with(['Admissao.Feedback' => function ($q) use ($empresa_id) {
-                            $q->withoutGlobalScopes()->select([
-                                'id',
-                                'curriculo_id',
-                                'empresa_id'
-                            ])->where('empresa_id', $empresa_id);
-                        }, 'Admissao.Feedback.Curriculo' => function ($qu) {
-                            $qu->withoutGlobalScopes()->select([
-                                'id',
-                                'nome',
-                                'nascimento',
-                                'rg'
-                            ]);
-                        }])
+                        ->whereHas('Admissao', function ($q) {
+                            $q->withoutGlobalScopes()->Admitidos();
+                        })
+                        ->select(['id', 'empresa_id', 'admissao_id', 'data_aso', 'data_vencimento'])
+                        ->where('data_vencimento', '>=', (new DataHora())->dataInsert())
+                        ->where('data_vencimento', '<=', $data->dataInsert())
+                        ->with('Admissao', function ($a) {
+                            $a->select(['id', 'feedback_id'])
+                                ->Admitidos()
+                                ->with('Feedback', function ($F) {
+                                    $F->withoutGlobalScopes()->select(['id', 'curriculo_id', 'empresa_id'])
+                                        ->with('Curriculo', function ($C) {
+                                            $C->withoutGlobalScopes()->select(['id', 'nome', 'nascimento', 'rg', 'orgao_expeditor']);
+                                        });
+                                });
+                        })
                         ->orderBy('data_vencimento')
                         ->get();
-                    foreach ($usuarios as $usuario) {
-                        $vencimentos = array();
-                        $dados = array();
 
-                        foreach ($AdmissoesAso as $vencimento) {
+                    $vencimentos = [];
+                    foreach ($AdmissoesAso as $vencimento) {
+                        $vencimentos[] = [
+                            'colaborador' => $vencimento->Admissao->Feedback->Curriculo->nome,
+                            'data_vencimento' => $vencimento->data_vencimento_formatada
+                        ];
+                    }
 
-                            $dados['colaborador'] = $vencimento->Admissao->Feedback->Curriculo->nome;
-                            $dados['data_vencimento'] = $vencimento->data_vencimento;
-
-                            if (!empty($dados)) {
-                                array_push($vencimentos, $dados);
-                            }
-                        }
-
-                        if (!empty($vencimentos)) {
-                            \Mail::send(new VencimentoAsoMail([
-                                'usuario' => $usuario,
-                                'vencimentos' => $vencimentos,
-                                'empresa_id' => $empresa_id,
-                            ]));
-                            \Log::info("E-mail de Vencimento ASO enviado com sucesso - {$usuario['nome']} - {$empresa_id}");
-
-                        }
+                    if (!empty($vencimentos)) {
+                        \App\Jobs\JobMailVencimentoAso::dispatch([
+                            'usuario' => $usuario,
+                            'vencimentos' => $vencimentos,
+                            'empresa_id' => $empresa_id,
+                        ])->delay(now()->addSeconds(5));
+                        \Log::info("E-mail de Vencimento ASO enviado com sucesso - {$usuario['nome']} - {$empresa_id}");
                     }
                 }
+
             }
         } catch (\Exception $e) {
-            return $e->getMessage();
-            \Log::error($e->getFile() . " - " . $e->getMessage() . " - " . $e->getCode() . ' Verifica Saida Ferias');
-        };
+            Log::debug("Erro ao processar Vencimento ASO - {$usuario['nome']} - {$empresa_id}");
+        }
     }
 }
