1f81c6ad247cef34aeb7585c00f994d9699db33c
Deyvisson Augusto <deyvisson@stc.ma.gov.br>
2022-07-16 01:08:27 -0300

    feat: removendo codigo obsoleto [MYBP-123]

--- resources/views/g/posadmissao/index.blade.php
+++ resources/views/g/posadmissao/index.blade.php
@@ -590,26 +590,6 @@
                     <i class="fas fa-file-excel"></i> EXPORTAR EXCEL <span class="badge badge-light"
                                                                            v-show="selecionados.length > 0">@{{ selecionados.length }}</span>
                 </button>
-                {{--                <form target="_blank"--}}
-                {{--                      action="{{ \App\Models\Sistema::UrlServidor }}/admissao/export/3hmMaxB0QB0zvE48exportsBGQG3bheYiaQP1cWIqdhPL1lbv5g9tWBnBhRUDIJCRFM2gqbZSALev3zPcZVbHlZS"--}}
-                {{--                      --}}{{--                      action="{{ route('admissao.excel') }}"--}}
-                {{--                      method="get">--}}
-                {{--                    @csrf--}}
-                {{--                    <input type="hidden" name="selecionados[]" v-for="item in selecionados" :value="item">--}}
-                {{--                    <input type="hidden" name="campoVaga" :value="controle.dados.campoVaga">--}}
-                {{--                    <input type="hidden" name="campoUf" :value="controle.dados.campoUf">--}}
-                {{--                    <input type="hidden" name="campoRh" :value="controle.dados.campoRh">--}}
-                {{--                    <input type="hidden" name="campoFinalRh" :value="controle.dados.campoFinalRh">--}}
-                {{--                    <input type="hidden" name="campoRota" :value="controle.dados.campoRota">--}}
-                {{--                    <input type="hidden" name="campoTecnica" :value="controle.dados.campoTecnica">--}}
-                {{--                    <input type="hidden" name="campoTeste" :value="controle.dados.campoTeste">--}}
-                {{--                    <input type="hidden" name="campoPcd" :value="controle.dados.campoPcd">--}}
-                {{--                    <button type="submit" class="btn btn-sm btn-primary ml-1"--}}
-                {{--                            :disabled="controle.carregando || (!controle.carregando && lista.length===0 && selecionados.length === 0 ) ">--}}
-                {{--                        <i class="fas fa-file-excel"></i> Exportar Excel <span class="badge badge-light"--}}
-                {{--                                                                               v-show="selecionados.length > 0">@{{ selecionados.length }}</span>--}}
-                {{--                    </button>--}}
-                {{--                </form>--}}
             </div>
         </div>
     </fieldset>
