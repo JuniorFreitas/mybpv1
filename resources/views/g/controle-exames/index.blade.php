883de4414aefd9144a4f47b9479552d38589b37e
juniorfreitas <joedejesusjunior@gmail.com>
2023-01-11 01:29:28 -0300

    MYBP-215: Ajuste no controle de exames

--- resources/views/g/controle-exames/index.blade.php
+++ resources/views/g/controle-exames/index.blade.php
@@ -228,11 +228,25 @@
                                             v-text="listaEmpresasExames.filter(item => item.id === form.empresa_exame_id)[0].dados.email"></h5>
                                     </div>
                                 </div>
+
+                                <div class="col-12" v-if="form.empresa_exame_id">
+                                    <div class="switchToggle">
+                                        <input type="checkbox" v-model="form.envia_email" id="envia_email_documentos">
+                                        <label for="envia_email_documentos">Enviar E-mail</label>
+                                    </div>
+                                </div>
+{{--                                <div class="col-2" v-if="form.empresa_exame_id">--}}
+{{--                                    <div class="switchToggle" v-show="whatsappLiberado">--}}
+{{--                                        <input type="checkbox" v-model="form.envia_whatsapp_documentos" id="envia_whatsapp_documentos">--}}
+{{--                                        <label for="envia_whatsapp_documentos">Enviar Whatsapp</label>--}}
+{{--                                    </div>--}}
+{{--                                </div>--}}
                             </div>
                         </fieldset>
 
                         <formulario :model='form' :formulario_id='2' v-if='form.formulario'
                                     :mostra_titulo='false'></formulario>
+
 
                     </div>
 
