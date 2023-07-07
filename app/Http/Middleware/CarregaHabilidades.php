e952c35b3ba6cc8db7d88bf15a9538b9cb2ed387
rodrigomedeiros <rodrigo.medeiros@mybp.com.br>
2023-07-06 21:33:27 -0300

    MYBP-335: Correção no cabeçalho do email, retirar o disabled da data do exame e retirar o cache das habilidades

--- app/Http/Middleware/CarregaHabilidades.php
+++ app/Http/Middleware/CarregaHabilidades.php
@@ -23,18 +23,11 @@ class CarregaHabilidades
             return redirect()->route('logout');
         }
 
-        $listaDeHabilidadesSistema = \Cache::get('habilidades_sistema');
-
-        if (is_null(\Cache::get('habilidades_sistema'))) {
-            $listaDeHabilidadesSistema = Habilidade::select('nome')->pluck('nome')->toArray();
-            \Cache::rememberForever('habilidades_sistema', function () use ($listaDeHabilidadesSistema) {
-                return $listaDeHabilidadesSistema;
-            });
-        }
+        $listaDeHabilidadesSistema = Habilidade::select('nome')->pluck('nome')->toArray(); // lista de habilidade do sistema
 
         foreach ($listaDeHabilidadesSistema as $habilidade) {
-            Gate::define($habilidade, function ($listaDeHabilidadesUsuario) use ($habilidade) {
-                if (collect($listaDeHabilidadesUsuario)->search($habilidade) !== false) {
+            Gate::define($habilidade, function (User $usuario) use ($habilidade) {
+                if (collect($usuario->listaDeHabilidades())->search($habilidade) !== false) {
                     return true;
                 }
                 return false;
