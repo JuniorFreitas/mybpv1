b60d77afdd57d22cbbb8629dbdc710002f7750c9
juniorfreitas <josedejesusjunior@gmail.com>
2024-01-08 09:14:02 -0300

    fix: ajuste

--- app/Http/Controllers/AdmissaoController.php
+++ app/Http/Controllers/AdmissaoController.php
@@ -1390,7 +1390,7 @@ class AdmissaoController extends Controller
                 'parecerTecnica:id,feedback_id,experiencia_cargas_rigger,opera_plat_movel,opera_plat_ponte',
                 'VagaAberta:id,empresa_id,vaga_id,titulo,municipio_id,ativo',
                 'VagaAberta.VagaSelecionada:id,nome,empresa_id,ativo',
-                'VagaAberta.Municipio:id,nome',
+                'VagaAberta.Municipio:id,nome,uf',
                 'Empresa:id,razao_social,cnpj,nome,cpf,area_id',
                 'Empresa.Area',
                 'VagaAberta.Projetos.Projeto'
@@ -1528,8 +1528,8 @@ class AdmissaoController extends Controller
             if ($item->admissao) {
                 $cc_colaborador = collect($cc['centros_custos'])->collapse()->where('id', $item->admissao->centro_custo_id)->first();
                 $item->admissao->emp_cnpj = null;
-                $item->admissao->emp_nome_fantasia = null;;
-                $item->admissao->emp_centro_custo = null;;
+                $item->admissao->emp_nome_fantasia = null;
+                $item->admissao->emp_centro_custo = null;
                 $item->admissao->emp_tipo = null;
 
                 if ($cc_colaborador) {
