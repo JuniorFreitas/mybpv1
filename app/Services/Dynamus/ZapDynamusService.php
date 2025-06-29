d00f6217e261721467fb5982c490fc3c84402608
juniorfreitas <josedejesusjunior@gmail.com>
2025-06-29 00:29:16 -0300

    ajuste no Whatsapp usando a evolution

--- app/Services/Dynamus/ZapDynamusService.php
+++ app/Services/Dynamus/ZapDynamusService.php
@@ -10,15 +10,18 @@ class ZapDynamusService
 {
     protected $client;
     protected $baseUrl;
+    protected $instancia;
 
     public function __construct()
     {
-        $this->baseUrl = 'https://zapapi.dynamusti.com.br/api/whatsapp/mybp-notificacao';
+        $this->instancia = 'mybp-notificacao';
+        $this->baseUrl = 'https://zapevolutionapi.dynamusti.com.br';
         $this->client = new Client([
             'timeout' => 30,
             'headers' => [
                 'Accept' => 'application/json',
                 'Content-Type' => 'application/json',
+                'apikey' => '20c5dee2915cfa8064a635434c118a60'
             ]
         ]);
     }
@@ -57,33 +60,63 @@ class ZapDynamusService
         }
     }
 
+    public function checkIsNumberWhatsapp(string $phone)
+    {
+        try {
+            $response = $this->client->post($this->baseUrl . "/chat/whatsappNumbers/" . $this->instancia, [
+                "numbers" => [$phone]
+            ]);
+
+            return json_decode($response->getBody()->getContents(), true);
+        } catch (GuzzleException $e) {
+            Log::error('Erro na verificação de número WhatsApp: ' . $e->getMessage());
+            throw $e;
+        }
+    }
+
     /**
      * Exemplo de método específico para o ZapDynamus
      */
     public function sendMessage(string $phone, string $message, array $attachment = [])
     {
-        return $this->post("send", [
+        $config = [
             'number' => $phone,
-            'message' => $message,
-            'attachment' => $attachment
-        ]);
+            'text' => $message,
+            'delay' => rand(2000, 4000)
+        ];
+
+        return $this->post("message/sendText/" . $this->instancia, $config);
     }
 
     public function sendImagem(string $phone, string $message, $path)
     {
-        return $this->post("send-image", [
-            'number' => $phone,
-            'caption' => $message,
-            'imagePath' => $path
+        return $this->post("message/sendMedia/" . $this->instancia, [
+//            'number' => $phone,
+//            'caption' => $message,
+//            'imagePath' => $path
+
+            "number" => $phone,
+            "mediatype" => "image",
+            "mimetype" => "image/png",
+            "caption" => $message,
+            "media" => $path,
+            "fileName" => "image.png",
+            'delay' => rand(2000, 4000),
+            "linkPreview" => true,
         ]);
     }
 
     public function sendPdf(string $phone, string $message, $path)
     {
         return $this->post("send-pdf", [
-            'number' => $phone,
-            'message' => $message,
-            'pdfPath' => $path
+            "number" => $phone,
+            "mediatype" => "image",
+            "mimetype" => "pdf",
+            "caption" => $message,
+            "media" => $path,
+            "fileName" => "image.pdf",
+            'delay' => rand(2000, 4000),
+            "linkPreview" => true,
         ]);
     }
 }
