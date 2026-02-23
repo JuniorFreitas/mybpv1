6b141b2fb9926d3c90c66e1345829536de4dffc5
Junior Freitas <josedejesusjunior@gmail.com>
2026-02-22 22:58:59 -0300

    refactor: format command scheduling for Avaliação de Experiência
    - Improved readability of the command scheduling for 'mybp:avaliacao-experiencia' by breaking it into multiple lines.
    - Ensured consistent formatting for better maintainability of the scheduling logic.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -61,7 +61,11 @@ class Kernel extends ConsoleKernel
             ->at('00:00')
             ->name('mybp_treinamento_vencimento')
             ->onOneServer();
-        $schedule->command('mybp:avaliacao-experiencia')->mondays()->at('00:00')->name('mybp_avaliacao_experiencia')->onOneServer();
+        $schedule->command('mybp:avaliacao-experiencia')
+        ->mondays()
+        ->at('00:00')
+        ->name('mybp_avaliacao_experiencia')
+        ->onOneServer();
 
     }
 
