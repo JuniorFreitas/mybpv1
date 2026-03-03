8e4f94ed805adfa1eb02f0f7a7286a8b17fa57e1
juniorfreitas <josedejesusjunior@gmail.com>
2026-03-03 00:03:45 -0300

    continue fluxo ajuste

--- app/Services/Log/LogTelegram.php
+++ app/Services/Log/LogTelegram.php
@@ -3,24 +3,24 @@
 namespace App\Services\Log;
 
 use MasterTag\TelegramBotHandler;
+use Monolog\LogRecord;
 
 class LogTelegram extends TelegramBotHandler
 {
-    public function write(array $record): void
+    protected function write(LogRecord $record): void
     {
         $userAutenticado = "USUÁRIO - Não autenticado";
         if (auth()->check()) {
             $userAutenticado = 'USUÁRIO - ' . auth()->user()->nome . ' - Empresa ' . auth()->user()->Empresa->razao_social;
         }
 
-        $mensagem = env('APP_NAME') . " | [{$record['level_name']}] - data: {$record['datetime']->format('d/m/Y H:i:s')} - mensagem: {$record['message']}";
-        if (isset($record['context']['exception'])) {
-            $mensagem .= " - Arquivo: {$record['context']['exception']->getFile()} - Linha: {$record['context']['exception']->getLine()}\n";
+        $mensagem = env('APP_NAME') . " | [{$record->level->getName()}] - data: {$record->datetime->format('d/m/Y H:i:s')} - mensagem: {$record->message}";
+        if (isset($record->context['exception'])) {
+            $mensagem .= " - Arquivo: {$record->context['exception']->getFile()} - Linha: {$record->context['exception']->getLine()}\n";
             $mensagem .= \Request::fullUrl() . "\n";
             $mensagem .= $userAutenticado;
         }
-        $record['formatted'] = $mensagem;
-        parent::write($record);
+        parent::write($record->with(formatted: $mensagem));
     }
 
 }
